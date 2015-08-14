a404cd16dc7a8eed1cd3efc048383351e4e72c9c
alanzhangyx <alanzhangyx@163.com>
2015-08-14 18:21:26 +0800

    自己测试下关于注解的使用

---
+++ src/test/java/ssmMvn/Action2.java
@@ -0,0 +1,9 @@
+package ssmMvn;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.SOURCE)
+public @interface Action2 {
+    
+}
---
+++ src/test/java/ssmMvn/ActionTest.java
@@ -0,0 +1,12 @@
+package ssmMvn;
+
+
+public class ActionTest {
+
+    @Action2
+    public static void main(String[] args) {
+        System.out.println(2);
+    }
+    
+
+}
